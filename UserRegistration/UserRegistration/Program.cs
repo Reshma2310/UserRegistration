1837ae055166f753c97a0e9e128f2cee0118e92c
Reshma2310 <dreshma253@gmail.com>
2022-06-04 13:09:55 +0500

    [Reshma] - Checking Last Name of User: Valid or not

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,7 +8,10 @@ namespace UserRegistration
             Console.WriteLine("Enter First Name of User");
             string FName = Console.ReadLine();
             RegexClass regex = new RegexClass();
-            regex.RegexName(FName);            
+            regex.RegexName(FName);
+            Console.WriteLine("Enter Last Name of User");
+            string LName = Console.ReadLine();
+            regex.RegexName(LName);
         }
     }
 }
