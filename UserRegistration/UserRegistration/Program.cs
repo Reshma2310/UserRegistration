8fe4e03b1f5122579aa6c64fd253e800fd30a9cb
Reshma2310 <dreshma253@gmail.com>
2022-06-04 12:38:24 +0500

    [Reshma] - Checking First Name of User: Valid or not

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name of User");
+            string FName = Console.ReadLine();
+            RegexClass regex = new RegexClass();
+            regex.RegexName(FName);            
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/RegexClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class RegexClass
+    {
+        const string EMAIL = "^[a-z]{3,}.[a-z]{3,}@[a-z]{2,}.[a-z]{2,}$";
+        const string NAME = "^[A-Z]{1}[a-z]{2,}$";       
+        public void RegexName(string Name)
+        {
+            if (Regex.IsMatch(Name, NAME))
+            {
+                Console.WriteLine("Input Name is Valid");
+                return;
+            }
+            Console.WriteLine("Input Name is Not Valid");
+        }        
+    }
+}
